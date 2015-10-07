64c1d8d1bc741ecfde60832eeb999ff54fc7722f
catscarlet <i@catscarlet.com>
2015-10-07 21:25:11 +0800

    updating

--- test.js
+++ test.js
@@ -2,13 +2,12 @@ a = 0;
 b = 0;
 c = 0;
 $( document ).ready( function() {
-  $( "#a" ).text( "aaa" );
 
-  $( "#button" ).click( function() {
+  $( "#button_test" ).click( function() {
     $( "input" ).each( function( k,v ) {
 
-      //alert( $( this ).attr( "id" ) + " " + $( this ).val(  ) );
-       $( this ).removeattr( "checked" );
+      alert( $( this ).attr( "id" ) + " " + $( this ).prop('checked'));
+
     } );
   $( "#b" ).text( b );
 } );} );
