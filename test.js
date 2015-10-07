16a5d1ccd4ae4d7c0cf3f2d3e370c0c0ce66a312
catscarlet <i@catscarlet.com>
2015-10-08 00:47:36 +0800

    add formStr for debuging

--- test.js
+++ test.js
@@ -1,44 +1,57 @@
 title = 'This it title';
 question = new Array();
 question[0] = new Array();
-question[0]['title'] = "This is Q0";
-question[0]['option0'] = "This is Q0 option0";
-question[0]['option1'] = "This is Q0 option1";
-question[0]['option2'] = "This is Q0 option2";
+question[0]['title'] = 'This is Q0';
 
+question[0]['option'] = new Array();
+question[0]['option'][0] = 'This is Q0 option0';
+question[0]['option'][1] = 'This is Q0 option1';
+question[0]['option'][2] = 'This is Q0 option2';
+/*
 question[1] = new Array();
-question[1]['title'] = "This is Q1";
+//question[1]['title'] = "This is Q1";
 question[1]['option0'] = "This is Q1 option0";
 question[1]['option1'] = "This is Q1 option1";
 question[1]['option2'] = "This is Q1 option2";
 
 question[2] = new Array();
-question[2]['title'] = "This is Q2";
+//question[2]['title'] = "This is Q2";
 question[2]['option0'] = "This is Q2 option0";
 question[2]['option1'] = "This is Q2 option1";
 question[2]['option2'] = "This is Q2 option2";
 
 question[3] = new Array();
-question[3]['title'] = "This is Q2";
+//question[3]['title'] = "This is Q2";
 question[3]['option0'] = "This is Q3 option0";
 question[3]['option1'] = "This is Q3 option1";
 question[3]['option2'] = "This is Q3 option2";
+*/
+questionNow = 0;
+formStr='';
 
-questionNow=0;
-
-$( document ).ready( function() {
+$(document).ready(function() {
 
   $('#title').html(title);
 
 
+for (var i = 0; i < question[0]['option'].length; i++) {
+  formStr = formStr + '<label for="option' + i + '">'+question[0]['option'][i]+'</label><input type="radio" name="option' + i + '" id="' + i + '" checked="checked" /><br>';
+}
+$('#optionForm').html(formStr);
 
-  $( "#button_test" ).click( function() {
-    $( "input" ).each( function( key,value ) {
 
 
+  $('#button_vote').click(function() {
+    alert(formStr);
+  });
+
+  $('#button_test').click(function() {
+    $('input').each(function(key, value) {
 
       //alert( $( this ).attr( "id" ) + " " + $( this ).prop('checked'));
 
-    } );
-  $( "#b" ).text( b );
-} );} );
+    });
+    $("#b").text(b);
+  })
+
+;});
